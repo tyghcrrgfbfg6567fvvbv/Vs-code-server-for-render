ee9f49bceb7d8334fc04c14cf8dd3363cc8f4d75
tyghcrrgfbfg6567fvvbv <risov700k@gmail.com>
2025-08-25 15:56:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
-# Download and extract code-server if not already present
-if [ ! -d "$HOME/.local/code-server" ]; then
-  mkdir -p ~/.local/bin
-  curl -fsSL https://github.com/coder/code-server/releases/download/v4.103.1/code-server-4.103.1-linux-amd64.tar.gz -o code-server.tar.gz
-  tar -xzf code-server.tar.gz
-  mv code-server-4.103.1-linux-amd64 ~/.local/code-server
-fi
+# Default password if none set in Render environment
+PASSWORD=${PASSWORD:-mysecret}
 
-export PATH=$HOME/.local/code-server/bin:$PATH
-
-# Render gives us $PORT automatically (default 10000)
+# Run code-server
 exec code-server \
   --bind-addr 0.0.0.0:${PORT:-10000} \
-  --auth PASSWORD \
+  --auth password \
   --disable-telemetry \
   --disable-update-check \
   .
