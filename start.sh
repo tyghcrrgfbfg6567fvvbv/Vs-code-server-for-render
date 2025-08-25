e1b8e530f2a2eba4857a2c3d2bf822da411e2491
tyghcrrgfbfg6567fvvbv <risov700k@gmail.com>
2025-08-25 16:09:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ set -e
 
 PASSWORD=${PASSWORD:-mysecret}
 
-# Run code-server
-exec code-server \
+# Run code-server from local install
+exec $HOME/.local/bin/code-server \
   --bind-addr 0.0.0.0:${PORT:-10000} \
   --auth password \
   --disable-telemetry \
